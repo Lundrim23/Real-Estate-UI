baeac549607f0be8c8f3cb2c470de0dc27e2a46b
Lundrim <aliulundrim22@gmail.com>
2024-04-11 22:30:52 +0200

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './layout.scss';
+import Navbar from './components/navBar/Navbar';
 
 function App() {
-  return <div className="layout">Hello</div>;
+  return (
+    <div className="layout">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
