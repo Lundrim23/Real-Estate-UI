3c95007ea0bdcf79770191cdac29063c8528da85
Lundrim <aliulundrim22@gmail.com>
2024-04-12 22:27:18 +0200

    Minimap added

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import './map.scss';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import Pin from '../pin/pin';
 
-function Map() {
-  return <div className="map">Map</div>;
+function Map({ items }) {
+  return (
+    <MapContainer
+      center={[51.4797, -1.90269]}
+      zoom={7}
+      scrollWheelZoom={false}
+      className="map"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {items.map((item) => (
+        <Pin item={item} key={item.id}></Pin>
+      ))}
+    </MapContainer>
+  );
 }
 
 export default Map;
