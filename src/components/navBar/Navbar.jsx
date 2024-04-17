fe364836be31ed5a3630c5a12650c02c0a081f60
Lundrim <aliulundrim22@gmail.com>
2024-04-17 23:36:24 +0200

    Final push

--- src/components/navBar/Navbar.jsx
+++ src/components/navBar/Navbar.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import './navbar.scss';
+import { Link } from 'react-router-dom';
 function Navbar() {
   const [open, setOpen] = useState(false);
+
+  const user = true;
+
   return (
     <nav>
       <div className="left">
@@ -15,10 +19,27 @@ function Navbar() {
         <a href="/"> Agents</a>
       </div>
       <div className="right">
-        <a href="/">Login</a>
-        <a href="/" className="register">
-          Register
-        </a>
+        {user ? (
+          <div className="user">
+            <img
+              src="https://images.pexels.com/photos/39866/entrepreneur-startup-start-up-man-39866.jpeg"
+              alt=""
+            />
+            <span>Jon Doe</span>
+            <Link to="/profile" className="profile">
+              <div className="notification">3</div>
+              <span>Profile</span>
+            </Link>
+          </div>
+        ) : (
+          <>
+            {' '}
+            <a href="/">Login</a>
+            <a href="/" className="register">
+              Register
+            </a>
+          </>
+        )}
         <div className="menuIcon">
           <img
             src="/menu.png"
--- src/routes/profilePage/ProfilePage.jsx
+++ src/routes/profilePage/ProfilePage.jsx
@@ -1,8 +1,50 @@
 import React from 'react';
+import Chat from '../../components/chat/chat';
+import List from '../../components/list/List';
 import './profilePage.scss';
 
 function ProfilePage() {
-  return <div className="profilepage">profilepage</div>;
+  return (
+    <div className="profilePage">
+      <div className="details">
+        <div className="wrapper">
+          <div className="title">
+            <h1>User Information</h1>
+            <button>Update Profile</button>
+          </div>
+          <div className="info">
+            <span>
+              Avatar:
+              <img
+                src="https://images.pexels.com/photos/39866/entrepreneur-startup-start-up-man-39866.jpeg"
+                alt=""
+              />
+            </span>
+            <span>
+              Username: <b>John Doe</b>
+            </span>
+            <span>
+              E-mail: <b>john@gmail.com</b>
+            </span>
+          </div>
+          <div className="title">
+            <h1>My List</h1>
+            <button>Create New Post</button>
+          </div>
+          <List />
+          <div className="title">
+            <h1>Saved List</h1>
+          </div>
+          <List />
+        </div>
+      </div>
+      <div className="chatContainer">
+        <div className="wrapper">
+          <Chat />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProfilePage;
